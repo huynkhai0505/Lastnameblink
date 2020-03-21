b4b0104e9a16b62398e21b7e15f6165313808c75
huynkhai0505 <53179392+huynkhai0505@users.noreply.github.com>
2020-03-21 22:15:02 +1100

    Add files via upload

---
+++ blink-an-led1.ino
@@ -0,0 +1,92 @@
+
+
+int led1 = D6;
+
+
+void setup() {
+
+  pinMode(led1, OUTPUT);
+  
+}
+
+void loop() {
+  // My last name is HUYNH
+  
+  //H
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+
+  //U
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(1500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  
+  //Y
+  digitalWrite(led1, HIGH);
+  delay(1500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(1500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(1500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  
+  //N
+  digitalWrite(led1, HIGH);
+  delay(1500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  
+  //H
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+  digitalWrite(led1, HIGH);
+  delay(500);
+  digitalWrite(led1, LOW);
+  delay(1000);
+}
